458dbd8644c2c7ff0902e0bf3835aeb74d02ad34
hamid19471 <hamid.19471@gmail.com>
2023-03-16 14:37:35 +0330

    Update: add base style useing tailwindcss for header and footer component

--- src/components/Layout/Footer/Footer.jsx
+++ src/components/Layout/Footer/Footer.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Footer = () => {
-    return <div>Footer</div>;
+    return (
+        <footer className="flex w-full absolute bottom-0 items-center justify-center bg-violet-600 py-4 mt-8 text-white text-sm font-light">
+            <p>© 2023 - All rights reserved</p>
+        </footer>
+    );
 };
 
 export default Footer;
--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Header = () => {
-    return <div>Header</div>;
+    return (
+        <header className="flex items-center justify-center py-8 bg-violet-600 text-white text-2xl font-bold mb-10">
+            <h1>Contact Application</h1>
+        </header>
+    );
 };
 
 export default Header;
