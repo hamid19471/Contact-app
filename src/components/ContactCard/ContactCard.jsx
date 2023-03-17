560cfd668c114eeef1d310c91657dc3ea8dff30a
hamid19471 <hamid.19471@gmail.com>
2023-03-17 22:14:02 +0330

    Update: add 2 buttons for deleter and edit, in this step delete is working

--- src/components/ContactCard/ContactCard.jsx
+++ src/components/ContactCard/ContactCard.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-const ContactCard = ({ name, email, phone }) => {
+const ContactCard = ({ name, email, phone, onDelete }) => {
     return (
-        <div className="flex flex-col max-w-[250px] items-center justify-center gap-5 bg-violet-500 py-8 text-white m-3 rounded-lg shadow-lg text-center">
+        <div className="flex flex-col max-w-[350px] items-center justify-center gap-5 bg-violet-500 py-8 text-white m-3 rounded-lg shadow-lg text-center">
             <div>
                 <p className="font-bold text-xl my-1">Name</p>
                 <span className="font-bold uppercase text-sm bg-slate-100 p-2 text-black rounded-lg">
@@ -20,6 +20,17 @@ const ContactCard = ({ name, email, phone }) => {
                 <span className="font-bold uppercase text-sm bg-slate-100 p-2 text-black rounded-lg">
                     {phone}
                 </span>
+            </div>
+            <div className="flex items-center justify-between w-full">
+                <button
+                    className="py-1 px-5 bg-red-600 font-bold mx-2 text-sm rounded-lg"
+                    onClick={onDelete}
+                >
+                    Deleter
+                </button>
+                <button className="py-1 px-5 bg-amber-400 font-bold mx-2 text-sm rounded-lg">
+                    Edit
+                </button>
             </div>
         </div>
     );
