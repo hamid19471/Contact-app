07f46dcf53804143c092f12318ed0f4b12ad6c23
hamid19471 <hamid.19471@gmail.com>
2023-03-18 01:52:43 +0330

    Update: add route link for add new contact in other page

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,18 +1,29 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import ContactCard from "../ContactCard/ContactCard";
 
 const ContactList = ({ contacts, onDelete }) => {
     return (
-        <div className="grid grid-cols-7 gap-3">
-            {contacts.map((item) => {
-                return (
-                    <ContactCard
-                        key={item.id}
-                        onDelete={() => onDelete(item.id)}
-                        {...item}
-                    />
-                );
-            })}
+        <div className="flex flex-col items-center">
+            <div>
+                <Link
+                    to="/add"
+                    className="py-2 px-6 bg-violet-500 text-white rounded-lg"
+                >
+                    Add New Contact
+                </Link>
+            </div>
+            <div className="grid grid-cols-7 gap-3">
+                {contacts.map((item) => {
+                    return (
+                        <ContactCard
+                            key={item.id}
+                            onDelete={() => onDelete(item.id)}
+                            {...item}
+                        />
+                    );
+                })}
+            </div>
         </div>
     );
 };
