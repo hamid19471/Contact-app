041199cf84e3117f45afe9d89a57356a28a33472
hamid19471 <hamid.19471@gmail.com>
2023-03-17 17:52:40 +0330

    Fix: some style issue

--- src/components/Layout/Footer/Footer.jsx
+++ src/components/Layout/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <footer className="flex w-full absolute bottom-0 items-center justify-center bg-violet-600 py-4 mt-8 text-white text-sm font-light">
+        <footer className="flex w-full fixed bottom-0 items-center justify-center bg-violet-600 py-4 mt-8 text-white text-sm font-light">
             <p>© 2023 - All rights reserved</p>
         </footer>
     );
