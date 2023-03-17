cfcfa680164bc1ce64daea04cab69e0d1d5a985f
hamid19471 <hamid.19471@gmail.com>
2023-03-17 17:53:59 +0330

    Update: use map to show new contact and use props from main contact component

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import ContactCard from "../ContactCard/ContactCard";
 
-const ContactList = () => {
-    return <div>ContactList</div>;
+const ContactList = ({ contacts }) => {
+    return (
+        <div className="grid grid-cols-7 gap-3">
+            {contacts.map((item) => {
+                return <ContactCard key={item.id} {...item} />;
+            })}
+        </div>
+    );
 };
 
 export default ContactList;
