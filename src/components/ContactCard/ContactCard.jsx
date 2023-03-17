9a338075e4730a8dd4530672c097609b4452e3e8
hamid19471 <hamid.19471@gmail.com>
2023-03-18 01:53:06 +0330

    Update: add links and route for better manage

--- src/components/ContactCard/ContactCard.jsx
+++ src/components/ContactCard/ContactCard.jsx
@@ -1,13 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const ContactCard = ({ name, email, phone, onDelete }) => {
+const ContactCard = ({ name, email, phone, id, onDelete }) => {
     return (
         <div className="flex flex-col max-w-[350px] items-center justify-center gap-5 bg-violet-500 py-8 text-white m-3 rounded-lg shadow-lg text-center">
             <div>
-                <p className="font-bold text-xl my-1">Name</p>
-                <span className="font-bold uppercase text-sm bg-slate-100 p-2 text-black rounded-lg">
-                    {name}
-                </span>
+                <Link to={`/contact/${id}`}>
+                    <p className="font-bold text-xl my-1">Name</p>
+                    <span className="font-bold uppercase text-sm bg-slate-100 p-2 text-black rounded-lg">
+                        {name}
+                    </span>
+                </Link>
             </div>
             <div>
                 <p className="font-bold text-xl my-1">E-Mail</p>
