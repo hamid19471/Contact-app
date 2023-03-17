2f6fd110791f2bc3c7377658898b41fe2d923730
hamid19471 <hamid.19471@gmail.com>
2023-03-18 01:51:53 +0330

    Update: add local storage and routs for add, list, contact detail

--- src/components/MainContactApp/MainContactApp.jsx
+++ src/components/MainContactApp/MainContactApp.jsx
@@ -1,27 +1,58 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Addcontact from "../Addcontact/Addcontact";
 import ContactList from "../ContactList/ContactList";
-
+import { Route, Routes, useNavigate } from "react-router-dom";
+import ContactSingle from "../ContactSingle/ContactSingle";
 const MainContactApp = () => {
     const [contacts, setContacts] = useState([]);
+    const navigate = useNavigate();
     const addContactHandler = (contact) => {
         setContacts([
             ...contacts,
             { id: Math.floor(Math.random() * 1000), ...contact },
         ]);
+        navigate("/");
     };
 
     const deleteContactHandler = (id) => {
-        const newContactList = contacts.filter((contact) => {
-            contact.id !== id;
-        });
+        const newContactList = contacts.filter((item) => item.id !== id);
         setContacts(newContactList);
     };
 
+    useEffect(() => {
+        const saveContact = localStorage.getItem("contacts");
+        if (saveContact) {
+            setContacts(JSON.parse(saveContact));
+        }
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem("contacts", JSON.stringify(contacts));
+    }, [contacts]);
+
     return (
         <div>
-            <Addcontact addContactHandler={addContactHandler} />
-            <ContactList contacts={contacts} onDelete={deleteContactHandler} />
+            <Routes>
+                <Route
+                    path="/contact/:id"
+                    element={<ContactSingle contacts={contacts} />}
+                />
+                <Route
+                    path="/"
+                    element={
+                        <ContactList
+                            contacts={contacts}
+                            onDelete={deleteContactHandler}
+                        />
+                    }
+                />
+                <Route
+                    path="/add"
+                    element={
+                        <Addcontact addContactHandler={addContactHandler} />
+                    }
+                />
+            </Routes>
         </div>
     );
 };
