642075b62b5d2526197d39e90e9ecf4e5ff804c3
hamid19471 <hamid.19471@gmail.com>
2023-03-22 01:17:07 +0330

    Update: remove static code for read, create, remove contact and use axios to fetch data from json server

--- src/components/MainContactApp/MainContactApp.jsx
+++ src/components/MainContactApp/MainContactApp.jsx
@@ -3,34 +3,38 @@ import Addcontact from "../Addcontact/Addcontact";
 import ContactList from "../ContactList/ContactList";
 import { Route, Routes, useNavigate } from "react-router-dom";
 import ContactSingle from "../ContactSingle/ContactSingle";
-import axios from "axios";
+import getContacts from "../../httpRequests/getContacts";
+import removeContact from "../../httpRequests/removeContact";
+import addContact from "../../httpRequests/addContact";
+
 const MainContactApp = () => {
     const [contacts, setContacts] = useState([]);
     const navigate = useNavigate();
-    const addContactHandler = (contact) => {
-        setContacts([
-            ...contacts,
-            { id: Math.floor(Math.random() * 1000), ...contact },
-        ]);
-        navigate("/");
+    const addContactHandler = async (contact) => {
+        try {
+            const { data } = await addContact(contact);
+            setContacts([...contacts, data]);
+            navigate("/");
+        } catch (error) {}
     };
 
     const deleteContactHandler = (id) => {
-        const newContactList = contacts.filter((item) => item.id !== id);
-        setContacts(newContactList);
+        try {
+            removeContact(id);
+            const newContactList = contacts.filter((item) => item.id !== id);
+            setContacts(newContactList);
+        } catch (error) {}
     };
 
     useEffect(() => {
         const getContactData = async () => {
-            const { data } = await axios.get("http://localhost:3001/contacts");
-            setContacts(data);
+            try {
+                const { data } = await getContacts();
+                setContacts(data);
+            } catch (error) {}
         };
         getContactData();
     }, []);
-
-    // useEffect(() => {
-    //     localStorage.setItem("contacts", JSON.stringify(contacts));
-    // }, [contacts]);
 
     return (
         <div>
